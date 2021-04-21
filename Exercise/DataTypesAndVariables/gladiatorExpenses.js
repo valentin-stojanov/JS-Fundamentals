96c076e4ae889f56c37939e2a6e79d6081bbdf91
valentin-stojanov <valentin_stojanov@abv.bg>
2021-04-21 12:56:28 +0300

    Added exercises

---
+++ Exercise/DataTypesAndVariables/gladiatorExpenses.js
@@ -0,0 +1,16 @@
+function gladiatorExpenses(lostFightsCount, helmetPrice, swordPrice, shieldPrice, armourePrice) {
+
+    let helmetCount = Math.trunc(lostFightsCount / 2);
+    let swordCount = Math.trunc(lostFightsCount / 3);
+    let shieldCount = Math.trunc(lostFightsCount / 6);//When both his sword and helmet are broken in the same lost fight - this is every 6 lost fight (2 * 3)
+    // let armourCount = Math.trunc(shieldCount / 2);
+    let armourCount = (shieldCount / 2);
+
+    let totalExpenses = (helmetCount * helmetPrice) +
+        (swordCount * swordPrice) +
+        (shieldCount * shieldPrice) +
+        (armourCount * armourePrice);
+
+    console.log(`Gladiator expenses: ${totalExpenses.toFixed(2)} aureus`);
+}
+// gladiatorExpenses(23,    12.50,    21.50,    40,    200    );
