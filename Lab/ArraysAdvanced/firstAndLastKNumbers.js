7f09a73d4afc352f613a51506990f62734dd94ae
valentin <valentin_stojanov@abv.bg>
2021-04-26 22:21:31 +0300

    Added lab exercises

---
+++ Lab/ArraysAdvanced/firstAndLastKNumbers.js
@@ -0,0 +1,8 @@
+function firstAndLastKNumbers(array){
+    let k = array.shift();
+    let firstK = array.slice(0, k);
+    let lastK = array.slice(-k);
+    console.log(firstK.join(" "));
+    console.log(lastK.join(" "));
+}
+// firstAndLastKNumbers([2, 7, 8, 9]);
